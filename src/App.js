c3e7da43ce4f8f39fa1fc1ad73b7d4b4d726ef4a
Caijialinxx <caijialinxx@foxmail.com>
2018-06-15 19:02:25 +0800

    初步界面

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import './App.css'
 import icon_undone from './icon/square.svg'
 import icon_delete from './icon/bin.svg'
+import icon_up from './icon/up.svg'
+import icon_down from './icon/down.svg'
+import icon_add from './icon/add.svg'
 
 var link = document.createElement('link')
 link.href = "//at.alicdn.com/t/font_709826_rpbfifb41x.css"
@@ -29,7 +32,17 @@ class App extends Component {
   }
   render() {
     let todos = this.state.todoList.map((item, index) => {
-      return <li><img src={icon_undone}/>{item.content}</li>
+      return <li>
+        <div className='item'>
+          <img id='status' src={icon_undone} />
+          <span>{item.content}</span>
+        </div>
+        <div className='options'>
+          <img id='up' src={icon_up} />
+          <img id='down' src={icon_down} />
+          <img id='delete' src={icon_delete} />
+        </div>
+      </li>
     })
 
     return (
