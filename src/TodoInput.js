24edd49a43f614c969fbdbdc7fc157b73e532b2e
Caijialinxx <caijialinxx@foxmail.com>
2018-06-16 14:23:36 +0800

    删除无用代码

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -3,10 +3,9 @@ import icon_add from './icon/add.svg'
 
 class TodoInput extends Component {
   render() {
-    console.log(this.props)
     return <div className={this.props.className}>
       <img id={this.props.id} src={icon_add} />
-      <input type="text" value={this.props.content} maxlength="40" placeholder="添加待办事项" />
+      <input type="text" value={this.props.content} maxLength="40" placeholder="添加待办事项" />
     </div>
   }
 }
