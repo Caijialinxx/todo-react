d1db728e2ca9f281cacd4b6c2aaff9368fc09fb2
Caijialinxx <caijialinxx@foxmail.com>
2018-06-20 19:33:46 +0800

    input绑定state及change事件，form绑定submit事件

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -3,6 +3,15 @@ import './UserDialog.css'
 import $ from 'jquery'
 
 class UserDialog extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      formData: {
+        email: '',
+        password: ''
+      }
+    }
+  }
   render() {
     return (
       <div className='userDialog-wrapper'>
@@ -13,27 +22,27 @@ class UserDialog extends Component {
             <a onClick={this.switchAction.bind(this)} className='active' href='javascript:void(0)'>登　录</a>
           </nav>
           <div className='form-wrapper'>
-            <form id='signupForm'>
+            <form id='signupForm' onSubmit={this.signUp.bind(this)}>
               <div>
                 <label>邮箱:</label>
-                <input type='text' placeholder='请设置邮箱作为登录账号' />
+                <input type='text' value={this.state.formData.email} onChange={this.changeFormData.bind(this)} placeholder='请设置邮箱作为登录账号' />
               </div>
               <div>
                 <label>密码:</label>
-                <input type='password' />
+                <input type='password' value={this.state.formData.password} onChange={this.changeFormData.bind(this)} />
               </div>
               <input id='signupBtn' type='submit' value='注册' />
             </form>
-            <form id='loginForm'>
+            <form id='loginForm' onSubmit={this.logIn.bind(this)}>
               <div>
                 <label>邮箱:</label>
-                <input type='text' placeholder='' />
+                <input type='text' value={this.state.formData.email} onChange={this.changeFormData.bind(this)} placeholder='' />
               </div>
               <div>
                 <label>密码:</label>
-                <input type='password' />
+                <input type='password' value={this.state.formData.password} onChange={this.changeFormData.bind(this)} />
               </div>
-              <div className='options'><a href='' >忘记密码？</a></div>
+              <div className='options'><a href='javascript:void(0)'>忘记密码？</a></div>
               <input id='loginBtn' type='submit' value='登录' />
             </form>
           </div>
@@ -44,7 +53,6 @@ class UserDialog extends Component {
   switchAction(e) {
     $(e.target).addClass('active')
     $(e.target).siblings().removeClass('active')
-    console.log($(e.target).html())
     if ($(e.target).html() === '注　册') {
       $('#signupForm').css({ display: 'flex' })
       $('#loginForm').css({ display: 'none' })
@@ -52,6 +60,14 @@ class UserDialog extends Component {
       $('#signupForm').css({ display: 'none' })
       $('#loginForm').css({ display: 'flex' })
     }
+  }
+  signUp(e) {}
+  logIn(e) {}
+  changeFormData(e) {
+    let state_copy = JSON.parse(JSON.stringify(this.state)),
+      key = e.target.type === 'text' ? 'email' : 'password'
+    state_copy.formData[key] = e.target.value
+    this.setState(state_copy)
   }
 }
 
