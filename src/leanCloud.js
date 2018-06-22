fdbc7113754cea2832f2e8d88b378bb75d31025f
Caijialinxx <caijialinxx@foxmail.com>
2018-06-22 13:18:09 +0800

    重整代码

--- src/leanCloud.js
+++ src/leanCloud.js
@@ -4,8 +4,6 @@ let APP_ID = '4Sd2Vkpni8g9EB3wVuTzFAvt-gzGzoHsz'
 let APP_KEY = 'YyPdXBvVa8Vj9k4lwRA7JdcL'
 AV.init({ appId: APP_ID, appKey: APP_KEY })
 
-export default AV
-
 export function signUp(email, password, successFn, errorFn) {
   let user = new AV.User()
   user.setUsername(email)
@@ -16,20 +14,22 @@ export function signUp(email, password, successFn, errorFn) {
     successFn.call(undefined, user)
   }, (error) => {
     switch (error.code) {
+      case -1:
+        errorFn.call(undefined, '请求被终止，请检查网络是否正确连接！')
+        return;
       case 125:
-        alert('无效的电子邮箱地址！请检查！')
-        break;
+        errorFn.call(undefined, '无效的电子邮箱地址！请检查！')
+        return;
       case 203:
-        alert('电子邮箱地址已被占用！')
-        break;
+        errorFn.call(undefined, '电子邮箱地址已被占用！请更换！')
+        return;
       case 218:
-        alert('密码无效，不允许空白密码！请重设！')
-        break;
+        errorFn.call(undefined, '密码无效，不允许空白密码！请重设！')
+        return;
       default:
-        alert(error)
-        break;
+        errorFn.call(undefined, error)
+        return;
     }
-    errorFn.call(undefined, error)
   })
 }
 
@@ -39,60 +39,59 @@ export function logIn(email, password, successFn, errorFn) {
     successFn.call(undefined, user)
   }, (error) => {
     switch (error.code) {
+      case -1:
+        errorFn.call(undefined, '请求被终止，请检查网络是否正确连接！')
+        return;
       case 210:
-        alert('账号或密码错误！请检查！')
-        break;
+        errorFn.call(undefined, '账号或密码错误！请检查！')
+        return;
       case 211:
-        alert('账号不存在！如果未注册请先注册。')
-        break;
+        errorFn.call(undefined, '账号不存在！如果未注册请先注册。')
+        return;
       case 216:
-        alert('电子邮箱未通过验证！请先验证再登录。')
-        break;
+        errorFn.call(undefined, '电子邮箱未通过验证！请先验证再登录。')
+        return;
       case 219:
-        alert('登录失败次数超过限制，请稍候再试！或者通过忘记密码重设密码。')
-        break;
+        errorFn.call(undefined, '登录失败次数超过限制，请稍候再试！或者通过忘记密码重设密码。')
+        return;
       default:
-        alert(error)
-        break;
+        errorFn.call(undefined, error)
+        return;
     }
-    errorFn.call(undefined, error)
   })
 }
 
-export function reset(email, successFn, errorFn){
-  AV.User.requestPasswordReset(email).then(()=>{
+export function reset(email, successFn, errorFn) {
+  AV.User.requestPasswordReset(email).then(() => {
     successFn.call(undefined)
-  }, (error)=>{
-    console.log(error.code)
+  }, (error) => {
     switch (error.code) {
+      case -1:
+        errorFn.call(undefined, '请求被终止，请检查网络是否正确连接！')
+        return;
       case 204:
-        alert('请提供注册时的电子邮箱！')
-        break;
+        errorFn.call(undefined, '请提供注册时的电子邮箱！')
+        return;
       case 205:
-        alert('查询不到该电子邮箱，请检查电子邮箱或者重新注册！')
-        break;
+        errorFn.call(undefined, '查询不到该电子邮箱，请检查电子邮箱或者重新注册！')
+        return;
       case 216:
-        alert('电子邮箱未通过验证！请先验证再登录。')
-        break;
+        errorFn.call(undefined, '电子邮箱未通过验证！请先验证再登录。')
+        return;
       case 219:
-        alert('登录失败次数超过限制，请稍候再试！或者通过忘记密码重设密码。')
-        break;
+        errorFn.call(undefined, '登录失败次数超过限制，请稍候再试！或者通过忘记密码重设密码。')
+        return;
       default:
-        alert(error)
-        break;
+        errorFn.call(undefined, error)
+        return;
     }
-    errorFn.call(undefined, error)
   })
 }
 
 export function getCurrentUser() {
   let user = AV.User.current()
-  console.log(user)
-  if (user) {
-    return getUserInfo(user)
-  } else {
-    return null
-  }
+  if (user) { return getUserInfo(user) }
+  else { return null }
 }
 
 export function logOut() {
