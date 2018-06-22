2774e4f79f9da0ee55ffd5c779312571462fd078
Caijialinxx <caijialinxx@foxmail.com>
2018-06-22 15:27:30 +0800

    重整代码并添加验证邮箱请求

--- src/leanCloud.js
+++ src/leanCloud.js
@@ -15,19 +15,19 @@ export function signUp(email, password, successFn, errorFn) {
   }, (error) => {
     switch (error.code) {
       case -1:
-        errorFn.call(undefined, '请求被终止，请检查网络是否正确连接！')
+        errorFn.call(undefined, `错误代码：${error.code}\n错误消息：请求被终止，请检查网络是否正确连接！`)
         return;
       case 125:
-        errorFn.call(undefined, '无效的电子邮箱地址！请检查！')
+        errorFn.call(undefined, `错误代码：${error.code}\n错误消息：无效的电子邮箱地址！请检查！`)
         return;
       case 203:
-        errorFn.call(undefined, '电子邮箱地址已被占用！请更换！')
+        errorFn.call(undefined, `错误代码：${error.code}\n错误消息：电子邮箱地址已被占用！请更换！`)
         return;
       case 218:
-        errorFn.call(undefined, '密码无效，不允许空白密码！请重设！')
+        errorFn.call(undefined, `错误代码：${error.code}\n错误消息：密码无效，不允许空白密码！请重设！`)
         return;
       default:
-        errorFn.call(undefined, error)
+        errorFn.call(undefined, `错误代码：${error.code}\n错误消息：${error}`)
         return;
     }
   })
@@ -40,22 +40,23 @@ export function logIn(email, password, successFn, errorFn) {
   }, (error) => {
     switch (error.code) {
       case -1:
-        errorFn.call(undefined, '请求被终止，请检查网络是否正确连接！')
+        errorFn.call(undefined, `错误代码：${error.code}\n错误消息：请求被终止，请检查网络是否正确连接！`)
         return;
       case 210:
-        errorFn.call(undefined, '账号或密码错误！请检查！')
+        errorFn.call(undefined, `错误代码：${error.code}\n错误消息：账号或密码错误！请检查！`)
         return;
       case 211:
-        errorFn.call(undefined, '账号不存在！如果未注册请先注册。')
+        errorFn.call(undefined, `错误代码：${error.code}\n错误消息：账号不存在！如果未注册请先注册。`)
         return;
       case 216:
-        errorFn.call(undefined, '电子邮箱未通过验证！请先验证再登录。')
+        errorFn.call(undefined, `错误代码：${error.code}\n错误消息：电子邮箱未通过验证，请先验证再登录！验证邮件已发送至您的邮箱（${email}），请转至邮箱查收并进行验证！`)
+        verify(email)
         return;
       case 219:
-        errorFn.call(undefined, '登录失败次数超过限制，请稍候再试！或者通过忘记密码重设密码。')
+        errorFn.call(undefined, `错误代码：${error.code}\n错误消息：登录失败次数超过限制，请稍候再试！或者通过忘记密码重设密码。`)
         return;
       default:
-        errorFn.call(undefined, error)
+        errorFn.call(undefined, `错误代码：${error.code}\n错误消息：${error}`)
         return;
     }
   })
@@ -67,22 +68,16 @@ export function reset(email, successFn, errorFn) {
   }, (error) => {
     switch (error.code) {
       case -1:
-        errorFn.call(undefined, '请求被终止，请检查网络是否正确连接！')
+        errorFn.call(undefined, `错误代码：${error.code}\n请求被终止，请检查网络是否正确连接！`)
         return;
       case 204:
-        errorFn.call(undefined, '请提供注册时的电子邮箱！')
+        errorFn.call(undefined, `错误代码：${error.code}\n请提供注册时的电子邮箱！`)
         return;
       case 205:
-        errorFn.call(undefined, '查询不到该电子邮箱，请检查电子邮箱或者重新注册！')
-        return;
-      case 216:
-        errorFn.call(undefined, '电子邮箱未通过验证！请先验证再登录。')
-        return;
-      case 219:
-        errorFn.call(undefined, '登录失败次数超过限制，请稍候再试！或者通过忘记密码重设密码。')
+        errorFn.call(undefined, `错误代码：${error.code}\n查询不到该电子邮箱，请检查电子邮箱是否输入正确或重新注册！`)
         return;
       default:
-        errorFn.call(undefined, error)
+        errorFn.call(undefined, `错误代码：${error.code}\n错误消息：${error}`)
         return;
     }
   })
@@ -90,7 +85,7 @@ export function reset(email, successFn, errorFn) {
 
 export function getCurrentUser() {
   let user = AV.User.current()
-  if (user) { return getUserInfo(user) }
+  if (user.emailVerified) { return getUserInfo(user) }
   else { return null }
 }
 
@@ -106,4 +101,17 @@ function getUserInfo(AVUser) {
     emailVerified: AVUser.attributes.emailVerified,
     username: AVUser.attributes.username
   }
+}
+
+function verify(email) {
+  AV.User.requestEmailVerify(email).then(() => { }, (error) => {
+    switch (error.code) {
+      case -1:
+        alert(`错误代码：${error.code}\n请求被终止，请检查网络是否正确连接！`)
+        return;
+      default:
+        alert(`错误代码：${error.code}\n错误消息：${error}`)
+        return;
+    }
+  })
 }
