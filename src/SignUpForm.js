18a633a685f94d2191f975bbdf94eb08d1647d12
Caijialinxx <caijialinxx@foxmail.com>
2018-06-23 12:33:36 +0800

    将SignUpForm改成一个函数默认导出

--- src/SignUpForm.js
+++ src/SignUpForm.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react';
 
-export default class SignUpForm extends Component {
-  render() {
-    return (
-      <form id='signupForm' onSubmit={this.props.onSubmit.bind(this)}>
-        <div>
-          <label>邮箱:</label>
-          <input type='text' value={this.props.formData.email} onChange={this.props.onChange.bind(this)} placeholder='请设置邮箱作为登录账号' />
-        </div>
-        <div>
-          <label>密码:</label>
-          <input type='password' value={this.props.formData.password} onChange={this.props.onChange.bind(this)} />
-        </div>
-        <div className='options'>
-          <input id='signupBtn' type='submit' value='注册' />
-        </div>
-      </form>
-    )
-  }
+export default function (props) {
+  return (
+    <form id='signupForm' onSubmit={props.onSubmit}>
+      <div>
+        <label>邮箱:</label>
+        <input type='text' value={props.formData.email} onChange={props.onChange} placeholder='请设置邮箱作为登录账号' />
+      </div>
+      <div>
+        <label>密码:</label>
+        <input type='password' value={props.formData.password} onChange={props.onChange} />
+      </div>
+      <div className='options'>
+        <input id='signupBtn' type='submit' value='注册' />
+      </div>
+    </form>
+  )
 }
