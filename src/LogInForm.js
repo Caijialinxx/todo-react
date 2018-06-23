af63e4d27537a6007fca84bfa03fa9ec3c075d77
Caijialinxx <caijialinxx@foxmail.com>
2018-06-23 12:33:17 +0800

    将LogInForm改成一个函数默认导出

--- src/LogInForm.js
+++ src/LogInForm.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class LogInForm extends Component {
-  render() {
-    return (
-      <form id='loginForm' onSubmit={this.props.onSubmit.bind(this)}>
-        <div>
-          <label>邮箱:</label>
-          <input type='text' value={this.props.formData.email} onChange={this.props.onChange.bind(this)} />
-        </div>
-        <div>
-          <label>密码:</label>
-          <input type='password' value={this.props.formData.password} onChange={this.props.onChange.bind(this)} />
-        </div>
-        <div className='options'>
-          <a onClick={this.props.onForget.bind(this)} href='javascript:void(0)'>忘记密码？</a>
-          <input id='loginBtn' type='submit' value='登录' />
-        </div>
-      </form>
-    )
-  }
+export default function (props) {
+  return (
+    <form id='loginForm' onSubmit={props.onSubmit}>
+      <div>
+        <label>邮箱:</label>
+        <input type='text' value={props.formData.email} onChange={props.onChange} />
+      </div>
+      <div>
+        <label>密码:</label>
+        <input type='password' value={props.formData.password} onChange={props.onChange} />
+      </div>
+      <div className='options'>
+        <a onClick={props.onForget} href='javascript:void(0)'>忘记密码？</a>
+        <input id='loginBtn' type='submit' value='登录' />
+      </div>
+    </form>
+  )
 }
