b7aa9edcce42dc29b1ab7f89174ad16e04d3df0a
Caijialinxx <caijialinxx@foxmail.com>
2018-06-23 13:00:34 +0800

    将TodoItem改成一个函数默认导出

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './TodoItem.css'
 import icon_undone from './icon/square.svg'
 import icon_done from './icon/square_ok.svg'
@@ -8,47 +8,41 @@ import icon_up from './icon/up.svg'
 import icon_down from './icon/down.svg'
 import icon_bottom from './icon/bottom.svg'
 
-class TodoItem extends Component {
-  render() {
-    let statusImg
-    if (this.props.todo.status === 'undone') {
-      statusImg = icon_undone
-    } else if (this.props.todo.status === 'done') {
-      statusImg = icon_done
-    }
+export default function (props) {
+  let statusImg = props.todo.status === 'undone' ? icon_undone : icon_done
 
-    return <li>
-        <div className='itemWrapper' onClick={this.toggle.bind(this)}>
-          <img src={statusImg} alt='status' />
-          <span className={this.props.todo.status}>{this.props.todo.content}</span>
-        </div>
-        <div className='optionsWrapper'>
-          <img src={icon_top} alt='top' onClick={this.top.bind(this)} />
-          <img src={icon_up} alt='up' onClick={this.up.bind(this)} />
-          <img src={icon_down} alt='down' onClick={this.down.bind(this)} />
-          <img src={icon_bottom} alt='bottom' onClick={this.bottom.bind(this)} />
-          <img src={icon_delete} alt='delete' onClick={this.delete.bind(this)} />
-        </div>
+  return (
+    <li>
+      <div className='itemWrapper' onClick={toggle.bind(null, props)}>
+        <img src={statusImg} alt='status' />
+        <span className={props.todo.status}>{props.todo.content}</span>
+      </div>
+      <div className='optionsWrapper'>
+        <img src={icon_top} alt='top' onClick={top.bind(undefined, props)} />
+        <img src={icon_up} alt='up' onClick={up.bind(undefined, props)} />
+        <img src={icon_down} alt='down' onClick={down.bind(undefined, props)} />
+        <img src={icon_bottom} alt='bottom' onClick={bottom.bind(undefined, props)} />
+        <img src={icon_delete} alt='delete' onClick={deleteItem.bind(undefined, props)} />
+      </div>
     </li>
-  }
-  toggle(e) {
-    this.props.onToggle.call(undefined, e.currentTarget, { todo: this.props.todo, undone: icon_undone, done: icon_done });
-  }
-  top(e) {
-    this.props.onMove.call(undefined, e.currentTarget, 'toTop')
-  }
-  up(e) {
-    this.props.onMove.call(undefined, e.currentTarget, 'moveUp')
-  }
-  down(e) {
-    this.props.onMove.call(undefined, e.currentTarget, 'moveDown')
-  }
-  bottom(e) {
-    this.props.onMove.call(undefined, e.currentTarget, 'toBottom')
-  }
-  delete() {
-    this.props.onDelete.call(undefined, this.props.todo)
-  }
+  )
 }
 
-export default TodoItem
+function toggle(props, e) {
+  props.onToggle(e.currentTarget, { todo: props.todo, undone: icon_undone, done: icon_done });
+}
+function top(props, e) {
+  props.onMove(e.currentTarget, 'toTop')
+}
+function up(props, e) {
+  props.onMove(e.currentTarget, 'moveUp')
+}
+function down(props, e) {
+  props.onMove(e.currentTarget, 'moveDown')
+}
+function bottom(props, e) {
+  props.onMove(e.currentTarget, 'toBottom')
+}
+function deleteItem(props) {
+  props.onDelete(props.todo)
+}
