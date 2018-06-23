9e5d21be2d57af981b09a957ff89c296dd0128be
Caijialinxx <caijialinxx@foxmail.com>
2018-06-23 13:55:59 +0800

    将ResetForm改成一个函数默认导出

--- src/ResetForm.js
+++ src/ResetForm.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class ResetForm extends Component {
-  render() {
-    return (
-      <form id='resetForm' onSubmit={this.props.onSubmit.bind(this)}>
-        <h1>重置密码</h1>
-        <div>
-          <label>邮箱:</label>
-          <input type='text' value={this.props.formData.email} onChange={this.props.onChange.bind(this)} placeholder='注册的邮箱（即登录账号）' />
-        </div>
-        <div className='options'>
-          <a onClick={this.props.onShow.bind(this)} href='javascript:void(0)'>返回登录</a>
-          <input id='sendEmailBtn' type='submit' value='发送重置密码邮件' />
-        </div>
-      </form>
-    )
-  }
+export default function (props) {
+  return (
+    <form id='resetForm' onSubmit={props.onSubmit}>
+      <h1>重置密码</h1>
+      <div>
+        <label>邮箱:</label>
+        <input type='text' value={props.formData.email} onChange={props.onChange} placeholder='注册的邮箱（即登录账号）' />
+      </div>
+      <div className='options'>
+        <a onClick={props.onShow} href='javascript:void(0)'>返回登录</a>
+        <input id='sendEmailBtn' type='submit' value='发送重置密码邮件' />
+      </div>
+    </form>
+  )
 }
