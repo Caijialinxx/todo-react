412365787f80ae888a225202e1f967857cd4c641
Caijialinxx <caijialinxx@foxmail.com>
2018-06-23 13:56:09 +0800

    将Scrollbar改成一个函数默认导出

--- src/Scrollbar.js
+++ src/Scrollbar.js
@@ -1,10 +1,6 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './Scrollbar.css'
 
-class Scrollbar extends Component {
-  render() {
-    return <div className='scrollWrapper'><div className='scrollBar'></div></div>
-  }
+export default function () {
+  return <div className='scrollWrapper'><div className='scrollBar'></div></div>
 }
-
-export default Scrollbar
