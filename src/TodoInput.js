080d0983b6b21299baa0125be30001c97b21794a
Caijialinxx <caijialinxx@foxmail.com>
2018-06-23 12:49:07 +0800

    将TodoInput改成一个函数默认导出

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './TodoInput.css'
 import icon_add from './icon/add.svg'
 
-class TodoInput extends Component {
-  render() {
-    return <div className='inputWrapper'>
+export default function (props) {
+  return (
+    <div className='inputWrapper'>
       <label htmlFor='add'><img src={icon_add} alt='add' /></label>
       <input id='add' type="text"
-        value={this.props.content}
-        onKeyUp={this.submit.bind(this)}
-        onChange={this.change.bind(this)}
+        value={props.content}
+        onKeyUp={submit.bind(null, props)}
+        onChange={change.bind(null, props)}
         maxLength="40" placeholder="添加待办事项" />
     </div>
-  }
-  submit(e) {
-    if (e.target.value.trim() !== '' && e.keyCode === 13) {
-      this.props.onSubmit.call()
-    }
-  }
-  change(e) {
-    this.props.onChange.call(undefined, e.target.value)
+  )
+}
+
+function submit(props, e) {
+  if (e.target.value.trim() !== '' && e.keyCode === 13) {
+    props.onSubmit()
   }
 }
 
-export default TodoInput
+function change(props, e) {
+  props.onChange(e.target.value)
+}
