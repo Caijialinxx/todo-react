910e436da4acfefcb7849778c95f716455574d0f
Caijialinxx <caijialinxx@foxmail.com>
2018-08-04 17:53:37 +0800

    优化组件的样式呈现及布局方式

--- src/Clock.js
+++ src/Clock.js
@@ -9,14 +9,32 @@ class Time extends React.Component {
     }
   }
   render() {
-    return <p className='time'>{this.state.date.toTimeString().substr(0, 8)}</p>
+    let day
+    switch (this.state.date.getDay()) {
+      case 1: day = '一'
+        break
+      case 2: day = '二'
+        break
+      case 3: day = '三'
+        break
+      case 4: day = '四'
+        break
+      case 5: day = '五'
+        break
+      case 6: day = '六'
+        break
+      default: day = '日'
+        break
+    }
+    let date = `${this.state.date.getMonth() + 1}月${this.state.date.getDate()}日 星期${day} `
+    return <p className='time'>{date + this.state.date.toTimeString().substr(0, 5)}</p>
   }
   componentDidMount() {
     this.timerID = setInterval(() => {
       this.setState({
         date: new Date()
       })
-    }, 1000)
+    }, 20000)
   }
   componentWillUnmount() {
     clearInterval(this.timerID);
